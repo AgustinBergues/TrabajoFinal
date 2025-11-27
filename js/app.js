93319d25e210cf83944d928628249df4d9923ec1
AgustinBergues <abergues@uade.edu.ar>
2025-11-26 21:34:33 -0300

    Version casi final

---
+++ js/app.js
@@ -0,0 +1,44 @@
+/* ================================
+   FORMULARIO VOLUNTARIADO
+==================================*/
+
+const form = document.getElementById("voluntarioForm");
+const formSection = document.getElementById("form-section");
+const successSection = document.getElementById("success-section");
+
+successSection.style.display = "none";
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    formSection.style.display = "none";
+    successSection.style.display = "block";
+
+    form.reset();
+});
+
+
+/* ================================
+   Calculadora de impacto
+==================================*/
+
+const input = document.getElementById('donacion');
+const web = document.getElementById('impact-web');
+const robot = document.getElementById('impact-robot');
+const conect = document.getElementById('impact-conect');
+
+const costo = {
+web: 1200,
+robot: 1500,
+conect: 800
+};
+
+function updateImpact(){
+const monto = Number(input.value) || 0;
+
+web.textContent = Math.floor(monto / costo.web);
+robot.textContent = Math.floor(monto / costo.robot);
+conect.textContent = Math.floor(monto / costo.conect);
+}
+
+input.addEventListener('input', updateImpact);
